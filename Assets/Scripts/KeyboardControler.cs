df846c4224015dfd81c0fabae4c5b44a064c8669
cly4auHo <arsen.dmitriev96@gmail.com>
2019-10-04 16:45:41 +0300

    first

---
+++ Assets/Scripts/KeyboardControler.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class KeyboardControler : MonoBehaviour
+{
+    private CharacterController charController;
+    [SerializeField] private float speed = 6.0f;
+    [SerializeField] private float jumpSpeed = 15.0f;
+    private float gravity = -9.8f;
+    private float terminalVelocity = -10.0f;
+    private float minFall = -1.5f;
+    private float vertSpeed;
+
+    void Start()
+    {
+        vertSpeed = minFall;
+        charController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+
+        movement = Vector3.ClampMagnitude(movement, speed);       
+        movement = transform.TransformDirection(movement);
+
+        if (charController.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                vertSpeed = jumpSpeed;
+            }
+            else
+            {
+                vertSpeed = minFall;
+            }
+        }
+        else
+        {
+            vertSpeed += gravity * 5 * Time.deltaTime;
+            if (vertSpeed < terminalVelocity)
+            {
+                vertSpeed = terminalVelocity;
+            }
+        }
+        movement.y = vertSpeed;
+        movement *= Time.deltaTime;
+        charController.Move(movement);
+    }
+}
---
+++ Assets/Scripts/MouseControler.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class MouseControler : MonoBehaviour
+{
+    [SerializeField] private RotationAxes axes = RotationAxes.MouseXAndY;
+
+    [SerializeField] private float sensitivityHor = 9.0f;
+    [SerializeField] private float sensitivityVert = 9.0f;
+
+    private float minimumVert = -45.0f;
+    private float maximumVert = 45.0f;
+
+    private float rotationX = 0;
+    void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        if (body != null)
+            body.freezeRotation = true;
+    }
+    void Update()
+    {
+        if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
+        }
+        else if (axes == RotationAxes.MouseY)
+        {
+            rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            rotationX = Mathf.Clamp(rotationX, minimumVert, maximumVert);
+
+            float rotationY = transform.localEulerAngles.y;
+
+            transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
+        }
+        else
+        {
+            rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            rotationX = Mathf.Clamp(rotationX, minimumVert, maximumVert);
+
+            float delta = Input.GetAxis("Mouse X") * sensitivityHor;
+            float rotationY = transform.localEulerAngles.y + delta;
+
+            transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
+        }
+    }
+}
+public enum RotationAxes
+{
+    MouseXAndY = 0,
+    MouseX = 1,
+    MouseY = 2
+}
